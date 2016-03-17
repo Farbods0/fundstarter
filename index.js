03c37b4d3de84c9c025698b1bb3f7edeb97a6565
Ubuntu <ubuntu@ip-172-31-35-44.us-west-2.compute.internal>
2016-03-17 20:57:02 +0000

    make it better

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 var http = require('http');
 var fs = require('fs');
-var index = 'index.html';
-var requestListener = function (req, res) {
-  res.writeHead(200);
-  res.end('Hello, World!\n');
-}
-
-var server = http.createServer(requestListener);
-server.listen(8080);
+var port = process.env.PORT || 8080
+http.createServer(function (request, response) {
+	fs.readFile('index.html', function(error, content) {
+		if (error) {
+			response.writeHead(500);
+			response.end();
+		}
+		else {
+			response.writeHead(200, { 'Content-Type': 'text/html' });
+			response.end(content, 'utf-8');
+		}
+	});
+	
+}).listen(port);
+console.log('Server running on port: ', port);
